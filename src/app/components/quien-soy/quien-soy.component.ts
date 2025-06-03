8091d3bb01a1a8868586dd807d30f8064fd3732d
JoacoCasasola <joaquincasasola29@gmail.com>
2025-06-03 19:13:53 -0300

    commit_QuienSoy

--- src/app/components/quien-soy/quien-soy.component.ts
+++ src/app/components/quien-soy/quien-soy.component.ts
@@ -6,6 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./quien-soy.component.css']
 })
 export class QuienSoyComponent {
-  titulo = 'Joaquin Casasola';
-  textoLargo = 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae suscipit quos iste animi neque sapiente odio magnam omnis ducimus soluta distinctio reprehenderit, aliquam iure laboriosam sed, culpa dolor adipisci aspernatur libero voluptates? Deleniti debitis itaque beatae sint numquam consequuntur quod?';
+  titulo = 'Joaquín Casasola';
+  presentacion = 'Soy un estudiante de la UTN Avellaneda apasionado por la tecnología, actualmente buscando mi primera oportunidad laboral en el mundo IT. Me considero una persona proactiva, con gran capacidad de aprendizaje y motivación para crecer profesionalmente en este campo.';
+  habilidades = 'Entre mis habilidades se encuentran: programación en JavaScript/TypeScript, Angular, HTML/CSS y conocimientos básicos de backend.';
+  juegoTitulo = 'Piedra, Papel o Tijeras';
+  juegoDescripcion = 'Piedra, Papel o Tijeras es un juego clásico de manos que he implementado digitalmente. Las reglas son simples: la piedra vence a las tijeras rompiéndolas, las tijeras vencen al papel cortándolo, y el papel vence a la piedra envolviéndola. En caso de que ambos jugadores elijan la misma opción, es un empate.';
+  juegoObjetivo = 'El objetivo de mi implementación fue practicar conceptos de programación como lógica condicional, manejo de estados y interacción con el usuario, todo dentro de un entorno Angular.';
 }
